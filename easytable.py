b171c3b9a8ced45423d3f20fc4082efba18a2d6c
Shawn Esseltine <shawnesseltine@hotmail.com>
2023-04-25 20:19:00 -0400

    refactor: cleanup implementation

--- easytable.py
+++ easytable.py
@@ -1,73 +1,69 @@
 class EasyTable:
-    def __init__(self, header=None, padding=4):
-        self.header = header
-        self.data = []
+    data = []
+    _hasHeader = False
+    _rowLength = None
+    _maxRowDataLength = None
+
+    def from_data_with_header(data, padding=4):
+        table = EasyTable(padding=padding)
+        table.add_all(data)
+        table._hasHeader = True
+        return table
+
+    def from_data(data, padding=4):
+        table = EasyTable()
+        table.add_all(data)
+        return table
+
+    def __init__(self, padding=4):
         self.pad = padding
 
     def __str__(self):
-        colWidth = dict()
+        if self.data == []:
+            return ""
 
-        if self.header:
-            for col in range(len(self.header)):
-                colWidth[col] = len(str(self.header[col])) + self.pad
-        if len(self.data) >= 1:
-            for rows in self.data:
-                for col in range(len(rows)):
-                    newWidth = len(str(rows[col])) + self.pad
-                    if col not in colWidth.keys():
-                        colWidth[col] = newWidth
-                    else:
-                        if newWidth > colWidth[col]:
-                            colWidth[col] = newWidth
-        breakLine = "+"
-        row = "|"
-        out = ""
-        
-        if self.header:
-            for col in range(len(self.header)):
-                breakLine += "-"*colWidth[col] + "+"
-                row +=  '{:{align}{width}}'.format(str(self.header[col]), align='^', width=colWidth[col]) + '|'
-            out += breakLine + "\n" + row + "\n" + breakLine
+        def gen_row_str(row):
+            formattedData = ['{:{align}{width}}'.format(str(datum), align='^', width=datumWidth)
+                             for datum, datumWidth
+                             in zip(row, self._maxRowDataLength)]
+            return f"|{'|'.join(formattedData)}|\n"
+
+        breakLine = f"+{'+'.join(['-'*datumWidth for datumWidth in self._maxRowDataLength])}+\n"
+        rows = breakLine.join([gen_row_str(row) for row in self.data])
+
+        return f"{breakLine}{rows}{breakLine}"
+
+    def set_header(self, row):
+        self._validate_row(row)
+        if self._hasHeader:
+            self.data[0] = row
         else:
-            for rows in self.data:
-                for col in range(len(rows)):
-                    breakLine += "-"*colWidth[col] + "+"
-            out += breakLine
+            self.data.insert(0, row)
+            self._hasHeader = True
 
-        for rows in self.data:
-            row = "|"
-            for col in range(len(rows)):
-                row +=  '{:{align}{width}}'.format(str(rows[col]), align='^', width=colWidth[col]) + '|'
-            out += "\n" + row
-        return out + "\n" + breakLine
-                
+    def add_all(self, rows):
+        for row in rows:
+            self.add_row(row)
 
-    def set_header(self, header):
-        pass
-            
-
-    def add_row(self, inList):
-        if self.header:
-            minLen = len(self.header)
-        elif self.data != []:
-            minLen = len(self.data[0])
-        else:
-            minLen = 0
-
-            
-        if type(inList) not in (list, tuple):
+    def _validate_row(self, row):
+        assert len(row) != 0, "Rows cannot be empty"
+        if type(row) not in (list, tuple):
             raise TypeError("Only lists/tuples can be added as rows")
-        elif len(inList) == 0:
-            raise IndexError("The row cannot be empty")
-        elif type(inList[0]) == list:
-            for rows in inList:
-                if len(rows) is (minLen):
-                    self.data.append(rows)
-                else:
-                    raise IndexError("The length of all rows must be equal to the header and other rows")
-        elif len(inList) is (minLen):
-            self.data.append(inList)
-        else:
-            raise IndexError("The length of all rows must be equal to the header and other rows")
+        self._verify_data_length(row)
+        self._set_max_row_datum_length(row)
 
-    
+    def add_row(self, row):
+        self._validate_row(row)
+        self.data.append(row)
+
+    def _verify_data_length(self, data):
+        if self._rowLength is None:
+            self._rowLength = len(data)
+
+        assert len(data) == self._rowLength, "All rows must have same length"
+
+    def _set_max_row_datum_length(self, row):
+        if self._maxRowDataLength is None:
+            self._maxRowDataLength = [0 for _ in range(self._rowLength)]
+        self._maxRowDataLength = [max(prevMax, newSize + self.pad) for prevMax, newSize
+                                  in zip(self._maxRowDataLength, [len(str(datum)) for datum in row])]
